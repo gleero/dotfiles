06793aca88dadc537ef3bf6369227de5fe63f900
Gleero <gleero@gmail.com>
2013-04-27 01:27:33 +0300

    Module "bash"

---
+++ modules/bash/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Authors:
+#		Vladimir Perekladov, http://perekladov.ru/
+
+# Dotfiles package installer
+# https://github.com/gleero/dotfiles
+
+source "$DOTPATH/inc/modinstall.sh"
+
