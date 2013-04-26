bfa3ad84a4c36dff0fd8095a98955bd87dcaaad9
Gleero <gleero@gmail.com>
2013-04-27 01:58:55 +0300

    Module "completionpack"

---
+++ modules/completionpack/install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Authors:
+#		Vladimir Perekladov, http://perekladov.ru/
+
+# Dotfiles package installer
+# https://github.com/gleero/dotfiles
+
+DEPENDENCES="bash-completion"
+source "$DOTPATH/inc/modinstall.sh"
+
+
