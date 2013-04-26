5e47500c3cd7f3b96e4fca92349d5a8c88fbf982
Gleero <gleero@gmail.com>
2013-04-27 02:12:09 +0300

    module "core"

---
+++ modules/core/install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Dotfiles package installer
+# https://github.com/gleero/dotfiles
+
+# Authors:
+#		Vladimir Perekladov, http://perekladov.ru/
+
+source "$DOTPATH/inc/modinstall.sh"
+
+if [ "$1" == "bash_profile" ]; then
+
+	echo "#!/bin/bash"
+	echo ''
+	echo 'export LC_CTYPE=ru_RU.UTF-8'
+	echo 'export LC_ALL=ru_RU.UTF-8'
+	echo "export DOTPATH=\"$DOTPATH\""
+	echo ''
+	echo 'PATH="$PATH:$DOTPATH/bin"'
+	echo ''
+	echo 'for i in "$DOTPATH/inc/lib*.sh" ; do'
+	echo '	source $i'
+	echo "done"
+	echo ''
+	echo 'for i in "$DOTPATH/private/*.sh" ; do'
+	echo '	source $i'
+	echo "done"
+
+fi
