fde95142fc61047583b0b3590fea02faac98f7b7
Gleero <gleero@gmail.com>
2013-04-27 02:56:56 +0300

    module "extended"

---
+++ modules/extended/install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Dotfiles package installer
+
+# Authors:
+#		Vladimir Perekladov, http://perekladov.ru/
+
+# https://github.com/gleero/dotfiles
+
+DEPENDENCES="phantomjs"
+
+source "$DOTPATH/inc/modinstall.sh"
