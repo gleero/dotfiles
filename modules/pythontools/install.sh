12101c50666c9078f4c27834a53eff3c893653c8
Gleero <gleero@gmail.com>
2013-04-27 17:39:51 +0300

    module pythontools

---
+++ modules/pythontools/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Dotfiles package installer
+#
+# Authors:
+#		Vladimir Perekladov, http://perekladov.ru/
+#
+# https://github.com/gleero/dotfiles
+
+DEPENDENCES="graphviz"
+source "$DOTPATH/inc/modinstall.sh"
