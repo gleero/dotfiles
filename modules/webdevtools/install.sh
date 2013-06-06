130b1701a33586e4caefeaf9ac95fb74a82ac86b
Gleero <gleero@gmail.com>
2013-06-06 09:45:00 +0300

    Модуль webdevtools

---
+++ modules/webdevtools/install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Dotfiles package installer
+#
+# Authors:
+#		Vladimir Perekladov, http://perekladov.ru/
+#
+# https://github.com/gleero/dotfiles
+
+DEPENDENCES="fontforge"
+
+source "$DOTPATH/inc/modinstall.sh"
+
